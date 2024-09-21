8aad630236950ce380dcf77a952f56c26fcf4f87
Your New Name <yournewemail@example.com>
2024-09-21 23:38:30 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MovieList from './components/MovieList';
-import MovieDetails from './components/MovieDetails';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router, Routes, and Route
+import MovieList from './components/MovieList'; // Import the MovieList component
+import MovieDetails from './components/MovieDetails'; // Import the MovieDetails component
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" exact component={MovieList} />
-          <Route path="/movie/:id" component={MovieDetails} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<MovieList />} /> {/* Use 'element' instead of 'component' */}
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
